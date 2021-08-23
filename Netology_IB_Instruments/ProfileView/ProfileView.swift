4444351d5c7112467bdaf38f5198a57aa5bd77a8
ShchegikhinPA <shchegolikhin_pa@mail.ru>
2021-08-23 21:50:32 +0300

    first commit

---
+++ Netology_IB_Instruments/ProfileView/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Павел Щеголихин on 22.08.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var infoTextView: UITextView!
+
+}
---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Павел Щеголихин on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 15, y: 100, width: view.bounds.width - 30, height: view.bounds.height - 200)
+            view.addSubview(profileView)
+        }
+    }
+
+
+}
+
